1cb385165a6e594ddeb84aa8dd7a75b697ea48b8
Subhadip Saha <32738809+subhadip-saha@users.noreply.github.com>
2022-02-15 13:04:12 +0530

    React update

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+ import Restaurant from "./component/Basics/Restaurant";
+
+const App = () => {
+  return <Restaurant />;
+};
+
+export default App;
---
+++ src/component/Basics/Restaurant.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./style.css";
+
+const Restaurant = () => {
+  return (
+      
+      <>
+      <h1>Restaurant</h1>
+      </>
+      
+  );
+};
+
+export default Restaurant;
