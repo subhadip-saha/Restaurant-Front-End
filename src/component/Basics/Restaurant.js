542160c7d69342396010068a8aa376080541f593
Subhadip Saha <32738809+subhadip-saha@users.noreply.github.com>
2022-02-15 22:06:01 +0530

    Update

--- src/component/Basics/Restaurant.js
+++ src/component/Basics/Restaurant.js
@@ -1,11 +1,27 @@
 import React from "react";
 import "./style.css";
+import Menu from "./menuApi";
 
 const Restaurant = () => {
   return (
       
       <>
-      <h1>Restaurant</h1>
+      <div className="card-container">
+        <div className="card">
+          <div className="card-body">
+            <span className="card-number card-circle subtle">1</span>
+            <span className="card-author subtle">Breakfast</span>
+            <h2 className="card-title">Maggi</h2>
+            <span className="card-description subtle">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            In, fuga quam sunt velit sed perspiciatis.
+            </span>
+            <div className="card-read">Read</div>
+         </div>
+      
+         <span className="card-tag subtle">Order Now</span>
+        </div>
+      </div>
       </>
       
   );
